556c114f36b21061d834807d1310e11db9c81a53
karman503 <chabkarman@gmail.com>
2025-11-05 19:37:45 +0300

    tous les pages sont en complets juste les dynamisme BDD

--- main.py
+++ main.py
@@ -18,6 +18,40 @@ def propos():
 def contact():
     return render_template("contact.html", title="Contact")
 
+@app.route("/connexion")
+def login():
+    return render_template("login.html", title="Connexion")
+
+@app.route("/deconnexion")
+def logout():
+    return render_template("logout.html", title="Déconnexion")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html", title="Dashboard")
+
+
+@app.route("/dashboard/adherents")
+def adherents():
+    return render_template("adherents.html", title="Adhérents")
+
+
+@app.route("/dashboard/emprunts")
+def emprunts():
+    return render_template("emprunts.html", title="Emprunts")
+
+
+@app.route("/dashboard/statistiques")
+def statistiques():
+    return render_template("statistiques.html", title="Statistiques")
+
+
+@app.route("/dashboard/parametres")
+def parametres():
+    return render_template("parametres.html", title="Paramètres")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
